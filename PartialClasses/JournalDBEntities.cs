6b67af037cc835b2ef1b0c6afb59d5c30dbb88e4
Zaid <ZZMingaliev@kai.ru>
2022-08-14 23:26:59 +0300

    Добавьте файлы проекта.

---
+++ PartialClasses/JournalDBEntities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingQualityAcademicWork.Models
+{
+    public partial class JournalDBEntities : DbContext
+    {
+        private static JournalDBEntities journalDBEntities;
+        public static JournalDBEntities GetContext()
+        {
+            if(journalDBEntities == null)
+            {
+                journalDBEntities = new JournalDBEntities();
+            }
+            return journalDBEntities;
+        }
+    }
+}
---
+++ Windows/AddingStudentWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AccountingQualityAcademicWork.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddingStudentWindow.xaml
+    /// </summary>
+    public partial class AddingStudentWindow : Window
+    {
+        private MainWindow mainWindow;
+        public AddingStudentWindow(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            CbGroup.ItemsSource = Models.JournalDBEntities.GetContext().Group.ToList();
+            this.mainWindow = mainWindow;
+        }
+
+        private void BnAddingStudent_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Models.JournalDBEntities.GetContext().Student.Add(new Models.Student() { Name = TbName.Text, Surname = TbSurname.Text, Patronymic = TbPatronymic.Text, Group = CbGroup.SelectedItem as Models.Group });
+                Models.JournalDBEntities.GetContext().SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                mainWindow.Show();
+                this.Hide();
+                MessageBox.Show("Студент добавлен");
+            }
+        }
+    }
+}
